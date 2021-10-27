525b498239b510f9657f9007d55cee6cc5589b01
lakshmi <lakshmireddy1625@gmail.com>
2021-10-27 16:53:02 +0530

    this is my first checking

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package com.example.demo.hello;
+
+public class Hello {
+
+	public void m1() {
+		System.out.println("hello world welcome");
+System.out.println("hello world welcome ............checking");
+	}
+	public static void main(String[] args) {
+		System.out.println("main method hello world");
+		Hello h=new Hello();
+		h.m1();
+	}
+	
+}
